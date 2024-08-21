eb2d579118662ed7e54481fe788cd4eff246c0d9
Paige Rider <perider@unc.edu>
2024-08-21 17:53:43 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first excercise in COMP110!"""
+
+__author__ = "730769342"
+
+
+def greet(name: str) -> str:
+    """A welcoming first dunction definition."""
+    return "Hello, " + name + "!"
